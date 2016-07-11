9104a37ca1d66649085bcfa48b25017fa1547491
yuanyuli <tian.zhang@yoai.com>
2016-07-11 15:43:26 +0800

    update Demo

--- Demo/Demo.js
+++ Demo/Demo.js
@@ -5,12 +5,21 @@ import React, {
 } from 'react';
 import {
   View,
-  Text
+  Text,
+  ScrollView,
+  PixelRatio
 } from 'react-native';
+
+
+let red =  Math.round(Math.random() * 255);
+let green =  Math.round(Math.random() * 255);
+let blue =  Math.round(Math.random() * 255);
+
+let randomColor = ('rgb('+ red +','+green+','+blue +')').toString();
 
 import PortableView from './Test';
 
-let SECTIONS = [{color: 'red'}, {color: 'green'}, {color: 'blue'}, {color: 'black'}, {color: 'yellow'}];
+let SECTIONS = [{color: 'red'}, {color: 'green'}, {color: 'blue'}, {color: 'yellow'}, {color: randomColor}];
 
 
 export default class Demo extends Component {
@@ -18,41 +27,80 @@ export default class Demo extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      row: 3
+      data: SECTIONS,
     };
   }
 
-  renderContent(obj,i) {
+  renderContent(obj, i) {
     return (
       <View
-        style={{marginLeft:20,marginTop:20,width:100,height:100,backgroundColor:obj.color}}
-
+        style={{marginLeft:20,marginTop:20,width:70,height:30,backgroundColor:obj.color,justifyContent:'center',alignItems:'center',borderWidth:1/PixelRatio.get(),borderRadius:15}}
       >
-        <Text> {obj.color} + {i} </Text>
+        <Text
+        style = {{color: 'white'}}
+        > # {i} # </Text>
       </View>
     )
   }
 
-  render () {
+  render() {
     return (
-      <PortableView
-        style={{flex:1,width:300,flexWrap:'wrap',justifyContent:'center',alignItems:'center',flexDirection:'row'}}
-        data = {SECTIONS}
-        renderContent = {this.renderContent.bind(this)}
-      />
+
+      <View>
+        <PortableView
+          style={{flex:1,width:375,flexWrap:'wrap',alignItems:'center',flexDirection:'row',backgroundColor:'#dbdbdb',height:500,}}
+          data={SECTIONS}
+          renderContent={this.renderContent.bind(this)}
+          positionHasBeenChanged={(position1,position2) => {
+
+          // console.log("position1" + JSON.stringify(position1));
+          // console.log("position2" + JSON.stringify(position2));
+          }}
+          setPanResponderHandler={(obj,position,index) => {
+          // console.log('正在成为响应者');
+          // console.log("obj" + JSON.stringify(obj));
+          // console.log("obj" + JSON.stringify(obj));
+          // console.log("position" + JSON.stringify(position));
+          // console.log("index" + index);
+          }}
+        />
+        <View
+        style = {{borderWidth:1/PixelRatio.get(),justifyContent:'center',alignItems:'center',alignSelf:'center',width:300,height:50,marginTop:20}}
+        >
+          <Text
+            style = {{fontSize:20}}
+            onPress={this.add.bind(this)}
+          >Add</Text>
+        </View>
+        <View
+          style = {{borderWidth:1/PixelRatio.get(),justifyContent:'center',alignItems:'center',alignSelf:'center',width:300,height:50,marginTop:20}}
+        >
+          <Text
+            style = {{fontSize:20}}
+            onPress={this.minus.bind(this)}
+          >Minus</Text>
+        </View>
+      </View>
     )
   }
 
-  // render() {
-  //   return (
-  //     <PortableView
-  //       data={days}
-  //       row={this.state.row}
-  //       changeRow={() => {
-  //       this.setState({row:this.state.row <4? this.state.row+1 : 2})
-  //       }}
-  //     >
-  //     </PortableView>
-  //   );
-  // }
+  add() {
+    let sections = this.state.data;
+    sections.splice(sections.length, 0, {color: this.getRandomColor()});
+    this.forceUpdate();
+  }
+
+  minus() {
+    let sections = this.state.data;
+    sections.splice(sections.length - 1, 1);
+    this.forceUpdate();
+  }
+
+  getRandomColor() {
+    let red =  Math.round(Math.random() * 255);
+    let green =  Math.round(Math.random() * 255);
+    let blue =  Math.round(Math.random() * 255);
+
+    return ('rgb('+ red +','+green+','+blue +')').toString();
+  }
 }
--- Demo/Test.js
+++ Demo/Test.js
@@ -8,90 +8,44 @@ export default class PortableView extends Component {
     this.state = {
       data: props.data,
       pan: new Animated.ValueXY({x: 0, y: 0}),
-      translateX1: new Animated.Value(0),
-      isChangePosition: false,
-      isMoving: false,
       currentPanIndex: 0
-    },
-      this.positions = [],
-      this.animations = {
-        duration: 300,
-        create: {
-          type: LayoutAnimation.Types.linear,
-        },
-        update: {
-          type: LayoutAnimation.Types.linear,
-          springDamping: 0.7,
-        },
-      };
+    };
+    this.positions = []
   }
 
   componentWillMount() {
     this._panResponder = PanResponder.create({
-      // 要求成为响应者：
       onStartShouldSetPanResponder: (evt, gestureState) => true,
       onStartShouldSetPanResponderCapture: (evt, gestureState) => true,
       onMoveShouldSetPanResponder: (evt, gestureState) => true,
       onMoveShouldSetPanResponderCapture: (evt, gestureState) => true,
-
       onPanResponderGrant: (evt, gestureState) => {
+
+        //reset pan
         this.state.pan.setOffset({x: this.state.pan.x._value, y: this.state.pan.y._value});
-        this.state.pan.setValue({x: 0, y: 0})
-
-        let {x0, y0} = gestureState;
-
-        for (let i = 0; i < this.positions.length; i++) {
-          let position = this.positions[i];
-          let {x, y, width, height} = position;
-
-          if ((x0 >= x && x0 <= x + width) && (y0 >= y && y0 <= y + height)) {
-            this.setState({currentPanIndex: i});
-            let pan = this.refs["pan" + i];
-            pan = this._panResponder.panHandlers
-          }
-        }
+        this.state.pan.setValue({x: 0, y: 0});
+        this.setPanReaponderhandler(evt, gestureState);
 
       },
       onPanResponderMove: (evt, gestureState) => {
+
         let {x0, y0, dx, dy} = gestureState;
 
         this.state.pan.x.setValue(gestureState.dx);
         this.state.pan.y.setValue(gestureState.dy);
 
-        let sections = this.state.data;
-        // if (!this.state.isMoving) {
-        //
-        //   for (let i = 0; i < this.state.data.length; i++) {
-        //     if (this.isInSquare(gestureState, this.positions[i])) {
-        //       this.setState({isMoving: true})
-        //       let pan = this.state.data[this.state.currentPanIndex];
-        //       let sections = this.state.data;
-        //       sections.splice(this.state.currentPanIndex, 1);
-        //       sections.splice(i, 0, pan);
-        //       this.forceUpdate();
-        //     }
-        //   }
-        // }
       },
-      onPanResponderTerminationRequest: (evt, gestureState) => true,
+
+      // do not release panResponder 
+      onPanResponderTerminationRequest: (evt, gestureState) => false,
       onPanResponderRelease: (evt, gestureState) => {
+
+        // reset pan
         this.state.pan.flattenOffset();
         this.state.pan.setValue({x: 0, y: 0});
+        // change position
+        this.updatePosition(evt, gestureState);
 
-        for (let i = 0; i < this.state.data.length; i++) {
-
-          if (this.isInSquare(gestureState, this.positions[i])) {
-
-            let pan = this.state.data[this.state.currentPanIndex];
-            let sections = this.state.data;
-
-            sections.splice(this.state.currentPanIndex, 1);
-            sections.splice(i, 0, pan);
-
-            this.forceUpdate();
-            this.positionHasBeenChanged();
-          }
-        }
 
       },
       onPanResponderTerminate: (evt, gestureState) => {
@@ -102,26 +56,35 @@ export default class PortableView extends Component {
     });
   }
 
-  positionHasBeenChanged() {
-    this.props.positionHasBeenChanged && this.props.positionHasBeenChanged({
-      position: this.positions[this.state.currentPanIndex],
-      index: this.state.currentPanIndex
-    }, {position: this.positions[i], index: i})
+  setPanReaponderhandler(evt, gestureState) {
+    let {x0, y0} = gestureState;
+
+    for (let i = 0; i < this.positions.length; i++) {
+      let position = this.positions[i];
+      let {x, y, width, height} = position;
+      if ((x0 >= x && x0 <= x + width) && (y0 >= y && y0 <= y + height)) {
+        this.setState({currentPanIndex: i});
+        let pan = this.refs["pan" + i];
+        pan = this._panResponder.panHandlers;
+        this.props.setPanResponderHandler && this.props.setPanResponderHandler(this.state.data[i], this.positions[i], i)
+      }
+    }
   }
 
-  isInSquare(gestureState, position) {
-    let {x0, y0, dx, dy} = gestureState;
-    let {x, y, width, height} = position;
+  updatePosition(evt, gestureState) {
+    for (let i = 0; i < this.state.data.length; i++) {
 
-    let endX = x0 + dx;
-    let endY = y0 + dy;
+      if (this.isInSquare(gestureState, this.positions[i])) {
+        let pan = this.state.data[this.state.currentPanIndex];
+        let sections = this.state.data;
 
-    if ((endX >= x && endX <= x + width) && (endY >= y && endY <= y + height)) {
-      return true;
-    } else {
-      return false;
+        sections.splice(this.state.currentPanIndex, 1);
+        sections.splice(i, 0, pan);
+
+        this.forceUpdate();
+        this.positionHasBeenChanged(i);
+      }
     }
-
   }
 
   handleLayout(event, index) {
@@ -145,9 +108,9 @@ export default class PortableView extends Component {
         <Animated.View
           key={i}
           ref={"pan" + i}
+          style={{...animatedStyle}}
           {...this._panResponder.panHandlers}
           onLayout={(event) => this.handleLayout(event,i)}
-          style={{...animatedStyle}}
         >
           {this.props.renderContent && this.props.renderContent(obj, i)}
         </Animated.View>
@@ -157,19 +120,40 @@ export default class PortableView extends Component {
     return (
 
       <View
-        style={{flex:1,width:300,flexWrap:'wrap',justifyContent:'center',alignItems:'center',flexDirection:'row'}}
+        {...this.props.style}
       >
         {content}
-        <Text onPress={this.onPress.bind(this)}>点击</Text>
       </View>
     )
   }
 
-  onPress() {
-    let sections = this.state.data;
-    let pan = sections.splice(0, 1);
-    this.forceUpdate();
+  // callback
+
+  positionHasBeenChanged(i) {
+    this.props.positionHasBeenChanged && this.props.positionHasBeenChanged({
+      position: this.positions[this.state.currentPanIndex],
+      index: this.state.currentPanIndex
+    }, {position: this.positions[i], index: i})
   }
+
+
+  // private
+
+  isInSquare(gestureState, position) {
+    let {x0, y0, dx, dy} = gestureState;
+    let {x, y, width, height} = position;
+
+    let endX = x0 + dx;
+    let endY = y0 + dy;
+
+    if ((endX >= x && endX <= x + width) && (endY >= y && endY <= y + height)) {
+      return true;
+    } else {
+      return false;
+    }
+
+  }
+
 
 }
 
@@ -177,5 +161,6 @@ export default class PortableView extends Component {
 PortableView.PropTypes = {
   renderContent: React.PropTypes.func,
   positionHasBeenChanged: React.PropTypes.func,
+  setPanResponderHandler: React.PropTypes.func
 };
 PortableView.defaultProps = {};
